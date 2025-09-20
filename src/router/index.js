c92ac68b29cbf1b8c2affa1afb2b6159e3bb52a3
justin <justin980626@gmail.com>
2025-09-20 12:53:48 +0800

    adress

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import SosTools from '../views/SosTools.vue'
 import LagrangeCal from '../views/LagrangeCal.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
